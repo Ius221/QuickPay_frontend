f29607c7e59575021657e311d0edcce23146af88
Ayush_Gupta <ayushkumar9783@gmail.com>
2025-12-31 12:32:29 +0530

    Using slot for clear code

---
+++ src/components/Ui/HeadingSlot.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>
+    <slot></slot>
+  </h1>
+</template>
+
+<style scoped>
+h1 {
+  text-transform: uppercase;
+  font-family: Alumni Sans Pinstripe;
+  font-size: 3.2rem;
+  font-weight: 100;
+  color: #f1f7f6;
+  margin-bottom: 20px;
+  line-height: 0.9;
+  letter-spacing: 2px;
+}
+</style>
