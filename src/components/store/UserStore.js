bffca7366111a64d1f6547041c6ca00553182a7f
Ayush_Gupta <ayushkumar9783@gmail.com>
2026-01-06 17:18:17 +0530

    protecting routes

--- src/components/store/UserStore.js
+++ src/components/store/UserStore.js
@@ -5,16 +5,17 @@ export const useUserStore = defineStore('user', () => {
     // STATE
     let token = ref(null)
     let username = ref(null)
-    let isLoggedIn = ref(false)
+    let login = ref(false)
     let accNo = ref(0)
     let money = ref(0)
 
     // GETTER
     const getToken = computed(() => token.value)
+    const isLoggedIn = computed(() => login.value)
     const getAllData = computed(() => {
         return {
             loginUser: username.value,
-            isLoggedIn: isLoggedIn.value,
+            isLoggedIn: login.value,
             jwtToken: token.value,
             accNo: accNo.value,
             availableMoney: money.value,
@@ -28,19 +29,19 @@ export const useUserStore = defineStore('user', () => {
         username.value = data.username
         accNo.value = data.accNo
         money.value = data.money
-        isLoggedIn.value = true
+        login.value = true
     }
 
 
     function logout() {
-        isLoggedIn.value = false
+        login.value = false
         token.value = null
         username.value = null
         accNo.value = null
         money.value = null
     }
 
-    return { token, username, isLoggedIn, accNo, getToken, setUser, money, logout, getAllData }
+    return { token, username, isLoggedIn, accNo, getToken, setUser, money, logout, getAllData, login }
 }, {
     persist: true
 })
--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/components/store/UserStore'
 
 import HomePage from '@/components/auth/HomePage.vue'
 import LoginPage from '@/components/auth/LoginPage.vue'
@@ -14,21 +15,51 @@ const routes = [
   { path: '/home', component: HomePage },
   { path: '/login', component: LoginPage },
   { path: '/signup', component: SignupPage },
-  { path: '/dashboard', component: DashboardPage },
+  {
+    path: '/dashboard',
+    component: DashboardPage,
+    meta: { requiresAuth: true }
+  },
   {
     path: '/wallet', children: [
-      { path: 'deposit', component: DepositPage },
-      { path: 'withdraw', component: WithdrawPage },
+      {
+        path: 'deposit',
+        component: DepositPage,
+        meta: { requiresAuth: true }
+      },
+      {
+        path: 'withdraw',
+        component: WithdrawPage,
+        meta: { requiresAuth: true }
+      },
     ]
   },
-  { path: '/transfer', component: TransferPage },
-  { path: '/:err', component: ErrorPage },
+  {
+    path: '/transfer',
+    component: TransferPage,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/:err',
+    component: ErrorPage
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   linkActiveClass: 'active',
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const userStore = useUserStore();
+
+    if (userStore.isLoggedIn) next();
+    else next('/login')
+  } else {
+    next();
+  }
 })
 
 export default router
